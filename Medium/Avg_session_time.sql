ae7d8f86a0e44028423759c4bc5343c0d89e19e3
V1v1-H <62068544+V1v1-H@users.noreply.github.com>
2023-11-02 10:26:02 +0100

    Update Avg_session_time.sql

--- Medium/Avg_session_time.sql
+++ Medium/Avg_session_time.sql
@@ -1,3 +1,17 @@
+/*
+
+https://platform.stratascratch.com/coding/10352-users-by-avg-session-time?code_type=1
+
+Problem : 
+
+Calculate each user's average session time. A session is defined as the time difference between a page_load and page_exit. 
+
+For simplicity, assume a user has only 1 session per day and if there are multiple of the same events on that day, consider only the latest page_load and earliest page_exit, with an obvious restriction that load time event should happen before exit time event . 
+
+Output the user_id and their average session time.
+
+*/
+
 with tab_load as (select user_id, max(timestamp) as max_load, action, date(timestamp) as d_date 
 from facebook_web_log
 where action='page_load'
