50a74fb65b7caeb4f31af49cc8f6bacb43144f01
V1v1-H <62068544+V1v1-H@users.noreply.github.com>
2023-11-02 10:30:49 +0100

    Update High_Salaries.sql

--- Medium/High_Salaries.sql
+++ Medium/High_Salaries.sql
@@ -1,3 +1,14 @@
+/*
+https://platform.stratascratch.com/coding/10353-workers-with-the-highest-salaries?code_type=1
+
+Problem :
+
+You have been asked to find the job titles of the highest-paid employees.
+
+Your output should include the highest-paid title or multiple titles with the same salary.
+
+*/
+
 select worker_title as best_paid_title
 from worker as w
 join title as t
